56d3a989c1ba8951dc5959d932cfb90402994680
Ashish Chander <ashishchander2402@gmail.com>
2026-08-12 10:35:58 +0530

    increasing the time of call out

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 
-  timeout: 60000,
+  timeout: 120000,
 
   headers: {
     "Content-Type": "application/json",
